248e4ef160a423351b638f9630d86cdfe8432061
Ana Elisa <anasiani254@hotmail.com>
2025-05-06 20:34:19 -0300

    Alterando o arquivo

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 
-from aula08.schemas import Message, UserSchema
+from schemas import Message, UserSchema, UserPublic, UserDB
+
+from http import HTTPStatus
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)  #'/' é um endpoint
+
+@app.get(
+    '/', status_code=HTTPStatus.OK, response_model=Message
+)  #'/' é um endpoint
 def read_root():
     return {'message': 'Olá Mundo!', 'Outra': 'Outra Mensagem!'}
 
-@app.post ('/users')
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    return user
+    user_with_id = UserDB(
+        id=len(database) + 1 ** user.model_dump()  # converte em lista
+    )
+    database.append(user_with_id)  # serve para inserir na lista
+    return user_with_id
