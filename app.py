a5a57ec27b53cc9c81d076fc7e0be4f29434244e
Ana Elisa <anasiani254@hotmail.com>
2025-05-06 17:15:43 -0300

    Mexendo com schemas

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from aula08.schemas import Message, UserSchema
+
 app = FastAPI()
 
 
-@app.get('/')  #'/' é um endpoint
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)  #'/' é um endpoint
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'Olá Mundo!', 'Outra': 'Outra Mensagem!'}
+
+@app.post ('/users')
+def create_user(user: UserSchema):
+    return user
