a8a977c7ea982e9ff842cddd5b9111d01fcc2332
Ana Elisa <anasiani254@hotmail.com>
2025-05-06 20:34:52 -0300

    Atualizando arquivo

--- test_app.py
+++ test_app.py
@@ -1,12 +1,34 @@
 from http import HTTPStatus
-from fastapi.testclient import TestClient # type: ignore
-from aula08.app import app
-client = TestClient(app)
+from fastapi.testclient import TestClient  # type: ignore
+from app import app
+import pytest
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
 
 def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)
 
-response = client.get('/')
+    response = client.get('/')
 
-assert response.status_code == HTTPStatus.OK
-assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user():
+    client = TestClient(app)
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'ana',
+            'email': 'ana@exemplo.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'ana',
+        'email': 'ana@exemplo.com',
+        'id': 1,
+    }
